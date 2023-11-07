5bcdc03229265bd0f835c31e244d6dd620c04b4f
“Rodrigo <rodrigobpzantunes2016@gmail.com>
2023-11-06 21:57:23 -0300

    refactor: SEO

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps } from "next";
 import Image from "next/image";
+import Head from "next/head";
 import Link from "next/link";
 import Stripe from "stripe";
 import { stripe } from "../lib/stripe";
@@ -15,21 +16,26 @@ interface SuccessProps {
 
 export default function Success({ costumerName, product }: SuccessProps) {
     return (
-      <SuccessContainer>
-        <h1>Compra efetuada</h1>
+      <>
+        <Head>
+          <title>Compra efetuada | Ignite Shop</title>
+          <meta name="robots" content="noindex" />
+        </Head>
+        <SuccessContainer>
+          <h1>Compra efetuada</h1>
+          <ImageContainer>
+            <Image src={product.imageUrl} width={120} height={110} alt="" />
+          </ImageContainer>
 
-        <ImageContainer>
-          <Image src={product.imageUrl} width={120} height={110} alt="" />
-        </ImageContainer>
+          <p>
+            Uhuul <strong>{costumerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
+          </p>
 
-        <p>
-          Uhuul <strong>{costumerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
-        </p>
-
-        <Link href="/">
-          Voltar ao catálogo
-        </Link>
-      </SuccessContainer>
+          <Link href="/">
+            Voltar ao catálogo
+          </Link>
+        </SuccessContainer>
+      </>
     )
   }
 
